3054391dd62eedf9f0c70123d20c6edb2d1558ee
GAGAAA <81124904+DrillND@users.noreply.github.com>
2021-03-31 14:13:25 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                string input = Console.ReadLine();
+                int changeinput = 50000;
+
+                try
+                {
+                    changeinput = int.Parse(input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+                finally
+                {
+                    Console.WriteLine(changeinput);
+                }
+            }
+        }
+
+    }
+}
